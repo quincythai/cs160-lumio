f191809f60e1045ab6712dab1fa9286296d87db5
Kexin <kexinc@umich.edu>
2025-12-04 12:11:58 -0800

    updated lib/atoms.ts

--- lib/atoms.ts
+++ lib/atoms.ts
@@ -1,4 +1,5 @@
 import { atom } from "jotai";
+import { atomWithStorage } from "jotai/utils";
 
 export type Shot = {
   id: string;
@@ -14,4 +15,4 @@ export type Shot = {
 export const SHOTS_STORAGE_KEY = "lumio_shots_v1";
 
 // Global shots store keyed by projectId
-export const shotsAtom = atom<Record<string, Shot[]>>({});
+export const shotsAtom = atomWithStorage<Record<string, Shot[]>>(SHOTS_STORAGE_KEY, {});
